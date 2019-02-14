59cdb3f92a79a3a4a19d46bf437ae726bfa89857
benholtzman <benh@ldeo.columbia.edu>
2019-02-14 17:14:09 +0000

    added command line queries and file writing to data

--- run_SHT.py
+++ run_SHT.py
@@ -3,9 +3,22 @@ import sys
 import time
 import numpy as np
 import pandas as pd
+import datetime as dt
 #import Adafruit_DHT as adht
 from sht_sensor import Sht
 # https://pypi.org/project/sht-sensor/
+
+# get the date: 
+day = str(dt.date.today())
+print(day)
+#var = input("Please enter something: ")
+#print("You entered " + str(var))
+yr_name = input('Enter your name (no spaces) for the data filename: ')
+record_dur = input('How long do you want to record for (in seconds)?: ')
+filename = 'y' + day + '_' + yr_name + '_' + record_dur + 's.pkl'
+print('The file name will be: ' + filename)
+path_to_datasaving = './data/'
+
 
 # ========================================
 # https://docs.microsoft.com/en-us/windows/iot-core/learn-about-hardware/pinmappings/pinmappingsrpi
@@ -19,8 +32,10 @@ T_C = sht.read_t()
 rh = sht.read_rh(T_C)
 dew_point = sht.read_dew_point(T_C, rh)
 print(T_C,rh,dew_point)
+print('=================================')
 print('======= RECORDING NOW! ==========')
-
+print('=================================')
+print('TIME...TEMP [C]...HUMIDITY [%]')
 # ========================================
 
 temp_ls = []
@@ -29,7 +44,7 @@ time_ls = []
 time0 = time.time()
 
 t_now=0.0
-t_stop = 21600.0
+t_stop = float(record_dur)
 while t_now<=t_stop:
     t = time.time()
     t_now = round(t-time0,4)
@@ -49,7 +64,7 @@ hum = np.array(hum_ls)
 # write to panda and pickle it !
 
 data = pd.DataFrame({'time_s':time_s, 'temp_C':temp_C, 'hum':hum})
-data.to_pickle('./y190209_test.pkl')
-
+#data.to_pickle('./y190209_test.pkl')
+data.to_pickle(path_to_datasaving + filename)
 
 
